1be7bc6c8e9a2382ed482ffbb7e0a8d94f8e5375
chokxy <xinyan12345@gmail.com>
2020-03-11 00:57:16 -0700

    Added index.html and index.css files

--- app/routes.py
+++ app/routes.py
@@ -3,20 +3,20 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
+@app.route('/student', methods=['GET', 'POST'])
+def student():
     user = {'username': 'Chok'}
     if request.method == 'POST':
         days=request.form.getlist('days')
         # database querying
         return render_template('termplan.html', days=days)
-    return render_template('home.html', title='Home', user=user)
+    return render_template('student.html', title='Home', user=user)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('home'))
+        return redirect(url_for('student'))
     return render_template('hello.html', form=form)
 
 # if __name__ == '__main__':
